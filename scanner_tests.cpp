bf44b2175628377d53239c7489de5569d9b6ae45
Brian Button <bbutton@agilestl.com>
2020-06-13 11:55:36 -0500

    Recognizes when a stream of characters stops by seeing a a non-alpha char. This test is more useful if I actually call it...

--- scanner_tests.cpp
+++ scanner_tests.cpp
@@ -58,6 +58,7 @@ int main(int argc, char ** argv) {
     SingleCharacterReturnedAsToken();
     TwoCharactersReturnedAsSingleToken();
     SingleCharacterFollowedByDigitReturnsSingleCharToken();
+    MultiCharacterStringTerminatedBySpaceReturnsToken();
 
     return 0;
 }
